17c95df76c7788d10ef20dd362624522abb9d14a
sanjureddy17 <166575411+sanjureddy17@users.noreply.github.com>
2026-07-19 17:34:33 +0530

    Added PUT and DELETE endpoints for tasks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -19,6 +20,13 @@ tasks = [
         "done": True
     }
 ]
+
+class TaskCreate(BaseModel):
+    title: str
+
+class TaskUpdate(BaseModel):
+        title: str
+        done: bool
 
 @app.get("/")
 def home():
@@ -43,6 +51,45 @@ def get_task(task_id: int):
     for task in tasks:
         if task["id"] == task_id:
             return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+
+@app.post("/tasks", status_code=201)
+def create_task(task: TaskCreate):
+
+    new_task = {
+        "id": len(tasks) + 1,
+        "title": task.title,
+        "done": False
+    }
+
+    tasks.append(new_task)
+
+    return new_task
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task: TaskUpdate):
+
+    for t in tasks:
+        if t["id"] == task_id:
+            t["title"] = task.title
+            t["done"] = task.done
+            return t
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for i, task in enumerate(tasks):
+        if task["id"] == task_id:
+            tasks.pop(i)
+            return
 
     raise HTTPException(
         status_code=404,
